3b7c341c5d4fc0446a1ebffad45844fba5327c62
June Castillote <june.castillote@gmail.com>
2025-07-31 14:48:46 +0800

    Update publish.ps1

--- .github/scripts/publish.ps1
+++ .github/scripts/publish.ps1
@@ -2,7 +2,6 @@
 $moduleFile = 'PS.M365UsageReport.psd1'
 $manifest = Test-ModuleManifest $moduleFile
 $modulePath = $manifest.ModuleBase
-# Get-ChildItem $modulePath
 
 Publish-Module -Path $modulePath `
     -NuGetApiKey $env:PSGALLERY_API_KEY `
