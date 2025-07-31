770625ddb14f9e29c9634b2d37519791a38cdfa4
June Castillote <june.castillote@gmail.com>
2025-07-31 12:47:11 +0800

    update prechecks and publish

--- .github/scripts/publish.ps1
+++ .github/scripts/publish.ps1
@@ -1,4 +1,4 @@
-$manifest = Get-ChildItem -Path . -Filter PS.M36UsageReport.psd1 -Recurse | Select-Object -First 1
+$manifest = Get-ChildItem -Path . -Filter PS.M365UsageReport.psd1 -Recurse | Select-Object -First 1
 
 Publish-Module -Path (Split-Path $manifest.FullName) `
     -NuGetApiKey $env:PSGALLERY_API_KEY `
