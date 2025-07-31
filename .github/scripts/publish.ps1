00ec13efdab478c64240ebad9a23db0a73fe6188
June Castillote <june.castillote@gmail.com>
2025-07-31 13:12:39 +0800

    update publish.ps1

--- .github/scripts/publish.ps1
+++ .github/scripts/publish.ps1
@@ -1,6 +1,6 @@
 (Get-Location).Path
 $manifest = Get-ChildItem -Path . -Filter PS.M365UsageReport.psd1 -Recurse | Select-Object -First 1
-tree
+(Get-ChildItem $manifest.Directory.ToString() -Recurse).FullName
 
 Publish-Module -Path $manifest.Directory.ToString() `
     -NuGetApiKey $env:PSGALLERY_API_KEY `
